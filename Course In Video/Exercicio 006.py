1f714b8d4e71825cca3a93562ed0a181c8f96aac
vithor5 <vitor.camarapd@gmail.com>
2022-11-27 11:04:37 -0300

    Correction E006

--- Course In Video/Exercicio 006.py
+++ Course In Video/Exercicio 006.py
@@ -4,4 +4,4 @@ n3 = 3 * n
 nn = n ** 2
 print("O dobro de {} é {}".format(n, n2))
 print("O triplo de {} é {}".format(n, n3))
-print("A raiz quadrada de {} é {}".format(n, nn))
+print("O quadrado de {} é {}".format(n, nn))
