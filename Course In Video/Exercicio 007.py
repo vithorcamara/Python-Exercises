42842f33d3be56d4f6393e93691585285c7933b9
vithor5 <vitor.camarapd@gmail.com>
2022-11-30 23:12:57 -0300

    Update E007-E009

---
+++ Course In Video/Exercicio 007.py
@@ -0,0 +1,4 @@
+n1 = int(input())
+n2 = int(input())
+m = (n1 + n2) / 2
+print("A média de {} + {} é {}".format(n1, n2, m))
---
+++ Course In Video/Exercicio 008.py
@@ -0,0 +1,4 @@
+mt = float(input())
+cm = mt * 100
+mm = mt * 1000
+print("{} metros é {} centimetros ou {} milimetros".format(mt, cm, mm))
